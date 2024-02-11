231d89a80561d290000c35e7cf92ecc95ac9215c
fhssouza <fhssouza@gmail.com>
2024-02-11 20:50:25 -0300

    Find Category by Id, exception handling

--- src/main/java/com/souzatech/dscatalogapi/services/CategoryService.java
+++ src/main/java/com/souzatech/dscatalogapi/services/CategoryService.java
@@ -3,11 +3,13 @@ package com.souzatech.dscatalogapi.services;
 import com.souzatech.dscatalogapi.dto.CategoryDto;
 import com.souzatech.dscatalogapi.entities.Category;
 import com.souzatech.dscatalogapi.repositories.CategoryRepository;
+import com.souzatech.dscatalogapi.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -21,5 +23,11 @@ public class CategoryService {
         return list.stream()
                 .map(CategoryDto::new)
                 .toList();
+    }
+
+    public CategoryDto findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found: " + id));
+        return new CategoryDto(entity);
     }
 }
