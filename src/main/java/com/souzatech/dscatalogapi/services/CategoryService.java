8873b6b9117d8e5f4037e0380e017beb56a39e54
fhssouza <fhssouza@gmail.com>
2024-02-12 17:41:50 -0300

    Update category

--- src/main/java/com/souzatech/dscatalogapi/services/CategoryService.java
+++ src/main/java/com/souzatech/dscatalogapi/services/CategoryService.java
@@ -3,7 +3,8 @@ package com.souzatech.dscatalogapi.services;
 import com.souzatech.dscatalogapi.dto.CategoryDto;
 import com.souzatech.dscatalogapi.entities.Category;
 import com.souzatech.dscatalogapi.repositories.CategoryRepository;
-import com.souzatech.dscatalogapi.services.exceptions.EntityNotFoundException;
+import com.souzatech.dscatalogapi.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,7 +28,7 @@ public class CategoryService {
 
     public CategoryDto findById(Long id) {
         Optional<Category> obj = repository.findById(id);
-        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found: " + id));
+        Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found: " + id));
         return new CategoryDto(entity);
     }
 
@@ -37,5 +38,17 @@ public class CategoryService {
         entity.setName(dto.getName());
         entity = repository.save(entity);
         return new CategoryDto(entity);
+    }
+
+    @Transactional
+    public CategoryDto update(Long id, CategoryDto dto) {
+        try {
+            Category entity = repository.getReferenceById(id);
+            entity.setName(dto.getName());
+            entity = repository.save(entity);
+            return new CategoryDto(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id no found " + id);
+        }
     }
 }
---
+++ src/main/java/com/souzatech/dscatalogapi/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.souzatech.dscatalogapi.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
