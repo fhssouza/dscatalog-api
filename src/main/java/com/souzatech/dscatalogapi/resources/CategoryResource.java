944ecf240b3aa3c2163d018a30ed5b94f5b93afe
fhssouza <fhssouza@gmail.com>
2024-02-12 12:03:29 -0300

    POST new Category

--- src/main/java/com/souzatech/dscatalogapi/resources/CategoryResource.java
+++ src/main/java/com/souzatech/dscatalogapi/resources/CategoryResource.java
@@ -4,11 +4,10 @@ import com.souzatech.dscatalogapi.dto.CategoryDto;
 import com.souzatech.dscatalogapi.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,6 +26,17 @@ public class CategoryResource {
     public ResponseEntity<CategoryDto> findById(@PathVariable Long id){
         CategoryDto dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDto> insert(@RequestBody CategoryDto dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
 }
